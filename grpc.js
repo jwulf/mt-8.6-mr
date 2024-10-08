4e5f92c7221f3052fa8ec80ca0eca003e827dc78
Josh Wulf <josh.wulf@camunda.com>
2024-10-08 23:24:04 +1300

    format

--- grpc.js
+++ grpc.js
@@ -8,5 +8,8 @@ const c8 = new Camunda8({})
 const zeebe = c8.getZeebeGrpcApiClient()
 
 zeebe
-  .deployResource({processFilename: './resources/process.bpmn', tenantId: 'green'})
+  .deployResource({
+      processFilename: './resources/process.bpmn', 
+      tenantId: 'green'
+    })
   .then(console.log)
